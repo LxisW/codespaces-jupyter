fe3b2b42d53077016c7cbbc642c07d839b301e96
LxisW <luis.wagner.luis@aol.de>
2024-01-26 16:22:39 +0100

    added some oop code with inheritance

--- Python/oop.py
+++ Python/oop.py
@@ -12,6 +12,31 @@ class Person:
         return data
 
 
-peter = Person(name="Peter", age=19, height=180)
-peter.say_hello()
-print(peter.get_data())
+class Animal:
+    def __init__(self, name) -> None:
+        self.name = name
+
+    def get_name(self):
+        return self.name
+
+    def speak(self):
+        raise NotImplementedError("Subclasses must implement this method")
+
+
+class Dog(Animal):
+    def speak(self):
+        return f"{self.name} says Woof!"
+
+
+class Cat(Animal):
+    def speak(self):
+        return f"{self.name} says Meow!"
+
+
+dog = Dog(name="Bello")
+print(dog.speak())
+print(dog.get_name())
+
+# peter = Person(name="Peter", age=19, height=180)
+# peter.say_hello()
+# print(peter.get_data())
